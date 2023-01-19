7a024b91445ec9f5c096224b4cb4ce8bed86caf4
KlaytonJr <klaytonjunior@protonmail.com>
2023-01-19 19:33:04 -0300

    Create Header, Hero and Footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import Footer from 'pages/Footer';
+import Header from 'pages/Header';
+import Home from 'pages/Home';
 import './App.css';
 
 function App() {
   return (
     <>
+        <Header />
+
+        <Home />
+
+        <Footer />
     </>
   );
 }
---
+++ src/pages/Context/index.js
@@ -0,0 +1,11 @@
+
+const Context = () => {
+
+    return (
+        <>
+            <h1>Context</h1>
+        </>
+    );
+}
+
+export default Context;
